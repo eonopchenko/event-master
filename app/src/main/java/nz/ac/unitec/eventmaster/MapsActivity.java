56e9f98ec3666ba5791c79fa559415db1b3b0b75
eonopchenko <jake.spb@gmail.com>
2017-09-22 12:07:22 +1200

    +Added button to update the Map +Added comments and warning messages

--- app/src/main/java/nz/ac/unitec/eventmaster/MapsActivity.java
+++ app/src/main/java/nz/ac/unitec/eventmaster/MapsActivity.java
@@ -6,12 +6,15 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,6 +22,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.Result;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -30,6 +34,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -49,12 +54,12 @@ import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private class StableArrayAdapter extends ArrayAdapter<String> {
+    private class MyArrayAdapter extends ArrayAdapter<String> {
 
         HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
 
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
+        public MyArrayAdapter(Context context, int textViewResourceId,
+                              List<String> objects) {
             super(context, textViewResourceId, objects);
             for (int i = 0; i < objects.size(); ++i) {
                 mIdMap.put(objects.get(i), i);
@@ -78,6 +83,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private double lat;
     private double lng;
     private String city;
+
+    ///--- View components ---///
+    ProgressBar progressBar;
+    FloatingActionButton button;
+    ListView listView;
+
+    ///--- App logic variables ---///
+    ArrayList<String> list;
+    FusedLocationProviderClient client;
+    Geocoder gcd;
+    RequestQueue queue;
+    ArrayList<Marker> markers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,17 +121,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap = googleMap;
 
-        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar = findViewById(R.id.progressBar);
+        button = findViewById(R.id.floatingActionButton);
+        listView = findViewById(R.id.listView);
 
-        final ListView listview = findViewById(R.id.list_view);
-        final ArrayList<String> list = new ArrayList<String>();
+        list = new ArrayList<>();
+        markers = new ArrayList<>();
 
-        ((SlidingUpPanelLayout)findViewById(R.id.slMap)).setScrollableView(listview);
+        /// Set info box view on list item click
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                markers.get(i).showInfoWindow();
+            }
+        });
 
-        final FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(getApplicationContext());
-        final Geocoder gcd = new Geocoder(getApplicationContext(), Locale.getDefault());
-        final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        /// Set ScrollableView property to simplify list navigation
+        ((SlidingUpPanelLayout)findViewById(R.id.slMap)).setScrollableView(listView);
 
+        /// Instantiate location services (LocationProvider to get current location,
+        /// Geocoder to convert location to geographical name, Volley to get data from service provider)
+        client = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+        gcd = new Geocoder(getApplicationContext(), Locale.getDefault());
+        queue = Volley.newRequestQueue(getApplicationContext());
+
+        /// Initiate map update on start
+        updateView();
+
+        /// Initiate map update on button click
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateView();
+            }
+        });
+    }
+
+    private void updateView() {
+
+        /// Run current location request
         LocationRequest req = new LocationRequest();
         req.setInterval(10000);
         req.setFastestInterval(5000);
@@ -129,98 +174,118 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         try {
             Task<Location> location = client.getLastLocation();
+            list.clear();
+            markers.clear();
+            progressBar.setVisibility(View.VISIBLE);
 
             location.addOnCompleteListener(new OnCompleteListener<Location>() {
                 @Override
                 public void onComplete(@NonNull Task<Location> task) {
-                    try {
-                        lat = task.getResult().getLatitude();
-                        lng = task.getResult().getLongitude();
-                        LatLng marker = new LatLng(lat, lng);
-                        mMap.addMarker(new MarkerOptions().position(marker).title("I'm here!").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_map_pin_dark_red)));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 10));
-                    } catch (NullPointerException e) {
-                        System.out.println("task = null");
-                    }
-                    System.out.println("lat = " + lat + ", lng = " + lng);
+                    Location res = task.getResult();
+                    if (task.isSuccessful() && (res != null)) {
 
-                    List<Address> addresses = null;
-                    try {
-                        addresses = gcd.getFromLocation(lat, lng, 1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    assert addresses != null;
-                    if (addresses.size() > 0) {
-                        city = addresses.get(0).getLocality();
-                        System.out.println(city);
-                        String url = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + city + "&apikey=EGOkWNJ4TAr11J6KzrEETI4CzyprJVc3";
-                        JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                                    @Override
-                                    public void onResponse(JSONObject response) {
-                                        progressBar.setVisibility(View.INVISIBLE);
-                                        try {
-                                            JSONObject embedded = response.getJSONObject("_embedded");
-                                            JSONArray events = embedded.getJSONArray("events");
-                                            int length = events.length();
-                                            for (int index = 0; index < length; index++) {
-                                                JSONObject event = events.getJSONObject(index);
-                                                String eventName = event.getString("name");
-                                                JSONObject dates = event.getJSONObject("dates");
-                                                JSONObject start = dates.getJSONObject("start");
-                                                String startDateTime = start.getString("dateTime");
+                        /// Save new coordinates, create a new marker
+                        lat = res.getLatitude();
+                        lng = res.getLongitude();
+                        LatLng coordinates = new LatLng(lat, lng);
+                        Marker marker = mMap.addMarker(new MarkerOptions().position(coordinates).title("I'm here!").icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_map_pin_dark_red)));
+                        marker.showInfoWindow();
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 10));
+                        System.out.println("lat = " + lat + ", lng = " + lng);
 
-                                                String currency = "";
-                                                String minPrice = "";
-                                                String maxPrice = "";
-                                                try {
-                                                    JSONArray priceRanges = event.getJSONArray("priceRanges");
-                                                    JSONObject priceRange = priceRanges.getJSONObject(0);
-                                                    currency = priceRange.getString("currency");
-                                                    minPrice = priceRange.getString("min");
-                                                    maxPrice = priceRange.getString("max");
-                                                } catch (JSONException e) {
+                        /// Use Geocoder to get the city
+                        List<Address> addresses = null;
+                        try {
+                            addresses = gcd.getFromLocation(lat, lng, 1);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
 
-                                                }
+                        /// Use city to get current events
+                        assert addresses != null;
+                        if (addresses.size() > 0) {
+                            city = addresses.get(0).getLocality();
+                            System.out.println(city);
+                            String url = "https://app.ticketmaster.com/discovery/v2/events.json?city=" + city + "&apikey=EGOkWNJ4TAr11J6KzrEETI4CzyprJVc3";
+                            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                                    (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                                        @Override
+                                        public void onResponse(JSONObject response) {
+                                            progressBar.setVisibility(View.INVISIBLE);
+                                            /// Parse JSON
+                                            try {
+                                                JSONObject embedded = response.getJSONObject("_embedded");
+                                                JSONArray events = embedded.getJSONArray("events");
+                                                int length = events.length();
+                                                for (int index = 0; index < length; index++) {
+                                                    JSONObject event = events.getJSONObject(index);
+                                                    String eventName = event.getString("name");
+                                                    JSONObject dates = event.getJSONObject("dates");
+                                                    JSONObject start = dates.getJSONObject("start");
+                                                    String startDateTime = start.getString("dateTime");
 
-                                                JSONObject embeddedEvent = event.getJSONObject("_embedded");
-                                                JSONArray venues = embeddedEvent.getJSONArray("venues");
-                                                int venuesLength = venues.length();
-                                                for(int venuesIndex = 0; venuesIndex < venuesLength; venuesIndex++) {
+                                                    String currency = "";
+                                                    String minPrice = "";
+                                                    String maxPrice = "";
+                                                    try {
+                                                        JSONArray priceRanges = event.getJSONArray("priceRanges");
+                                                        JSONObject priceRange = priceRanges.getJSONObject(0);
+                                                        currency = priceRange.getString("currency");
+                                                        minPrice = priceRange.getString("min");
+                                                        maxPrice = priceRange.getString("max");
+                                                    } catch (JSONException e) {
 
-                                                    JSONObject venue = venues.getJSONObject(venuesIndex);
-                                                    JSONObject venueLocation = venue.getJSONObject("location");
+                                                    }
 
-                                                    double venueLng = Double.parseDouble(venueLocation.get("longitude").toString());
-                                                    double venueLat = Double.parseDouble(venueLocation.get("latitude").toString());
+                                                    JSONObject embeddedEvent = event.getJSONObject("_embedded");
+                                                    JSONArray venues = embeddedEvent.getJSONArray("venues");
+                                                    int venuesLength = venues.length();
+                                                    for (int venuesIndex = 0; venuesIndex < venuesLength; venuesIndex++) {
 
-                                                    if((venueLng != 0d) && (venueLat != 0d)) {
-                                                        LatLng venueMarker = new LatLng(venueLat, venueLng);
-                                                        mMap.addMarker(new MarkerOptions().position(venueMarker).title(eventName).snippet(startDateTime + " " + minPrice + currency + " - " + maxPrice + currency).icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_map_pin_azure)));
-                                                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(venueMarker, 10));
-                                                        list.add(eventName + " (price from " + minPrice + currency + " to " + maxPrice + currency + ", start at " + startDateTime + ")");
+                                                        JSONObject venue = venues.getJSONObject(venuesIndex);
+                                                        JSONObject venueLocation = venue.getJSONObject("location");
+
+                                                        double venueLng = Double.parseDouble(venueLocation.get("longitude").toString());
+                                                        double venueLat = Double.parseDouble(venueLocation.get("latitude").toString());
+
+                                                        /// Create a new marker, add to marker list and to list for list view adapter
+                                                        if ((venueLng != 0d) && (venueLat != 0d)) {
+                                                            LatLng venueCoordinates = new LatLng(venueLat, venueLng);
+                                                            Marker marker = mMap.addMarker(new MarkerOptions().position(venueCoordinates).title(eventName).snippet(startDateTime + " " + minPrice + currency + " - " + maxPrice + currency).icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_map_pin_azure)));
+                                                            markers.add(marker);
+                                                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(venueCoordinates, 10));
+                                                            list.add(eventName + " (price from " + minPrice + currency + " to " + maxPrice + currency + ", start at " + startDateTime + ")");
+                                                        }
                                                     }
                                                 }
+
+                                                /// When the list is filled, set list view adapter
+                                                final MyArrayAdapter adapter = new MyArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+                                                listView.setAdapter(adapter);
+
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            } finally {
+                                                progressBar.setVisibility(View.INVISIBLE);
                                             }
-
-                                            final StableArrayAdapter adapter = new StableArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
-                                            listview.setAdapter(adapter);
-
-                                        } catch (JSONException e) {
-                                            e.printStackTrace();
-                                        } finally {
-                                            progressBar.setVisibility(View.INVISIBLE);
                                         }
-                                    }
-                                }, new Response.ErrorListener() {
-                                    @Override
-                                    public void onErrorResponse(VolleyError error) {
+                                    }, new Response.ErrorListener() {
+                                        @Override
+                                        public void onErrorResponse(VolleyError error) {
 
-                                    }
-                                });
+                                        }
+                                    });
 
-                        queue.add(jsObjRequest);
+                            queue.add(jsObjRequest);
+                        }
+                    } else {
+                        /// @todo For some reasons after the first start, App fails to
+                        /// @todo get the current location
+                        progressBar.setVisibility(View.INVISIBLE);
+                        Toast.makeText(getApplicationContext(),
+                                "Failed to get location",
+                                Toast.LENGTH_LONG).show();
+                        System.err.println("Failed to get location");
                     }
                 }
             });
